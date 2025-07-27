44f27634ab2a873e67216e88dda2d1c7f176d073
KonTim25 <ktimch@gmail.com>
2025-07-27 12:18:51 +0300

    edit folder Cafeinfo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import css from "./App.module.css";
-import CafeInfo from "../Cafeinfo/CafeInfo";
+import CafeInfo from "../CafeInfo/CafeInfo";
 import { type Votes, type VoteType } from '../../types/votes';
 import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from '../VoteStats/VoteStats';
